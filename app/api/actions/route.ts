b7a359a77efd2404a62bff2c5f7a8702ef96357d
Sachiik0 <mmasanjuan@tip.edu.ph>
2024-07-20 18:28:28 +0800

    update label

--- app/api/actions/route.ts
+++ app/api/actions/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request){
             icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLT7i1KaPG3JWvgdPnWK9h3h3IxUxumVQsEA&s",
             description: "''Palimos'' is a Filipino term that refers to begging or the act of asking for alms or charity, typically on the streets or in public places. It often involves individuals or groups asking passersby for money or food to meet their immediate needs. Palimos is a common sight in many urban areas in the Philippines, where poverty and economic hardships may drive people to seek assistance from others.",
             title: "Online Limos for Solana Breakpoint 2024",
-            label:''
+            label:'Donate'
         }
         return Response.json(payload, {headers: ACTIONS_CORS_HEADERS,});
     }catch(err) {
